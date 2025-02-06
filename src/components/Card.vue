a7d7be4da7ab601a5c2b23ae73bed1b201ae419d
Kyi Win Htin <kyiwinhtin.kyiwinhtin@gmail.com>
2025-02-06 16:58:57 +0700

    Added a vue app

---
+++ src/components/Card.vue
@@ -0,0 +1,18 @@
+<script setup>
+	import { defineProps } from 'vue';
+
+	const props = defineProps({
+		bg : {
+			type : String,
+			default: 'bg-gray-100'
+		}
+	});
+
+	console.log(props.bg); 
+</script>
+
+<template>
+	<div :class="`${bg} p-6 rounded-lg shadow-md`">
+		<slot></slot>
+	</div>
+</template>
